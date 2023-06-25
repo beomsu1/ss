f4bee3d2898730fcce17bf5c99290e2f434e2639
Beomsu <beumsu1221@gmail.com>
2023-06-25 23:42:12 +0900

    spring security 0625

---
+++ src/test/java/org/bs/ss/SsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bs.ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
